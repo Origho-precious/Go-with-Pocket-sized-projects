f16cd98a5eb0b836479c16411d7320be561fb57b
Origho-precious <Origho9@gmail.com>
2023-12-28 02:05:51 +0100

    chore: adding multi-language support to the  hello program

--- hello/main.go
+++ hello/main.go
@@ -1,13 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-func main(){
-	greeting := greet()
-	
-	fmt.Println(greeting)
+type language string
+
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",             // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+	"de": "Hallo Welt", // German
 }
 
-func greet() string {
-	return "Hello, World!"
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+
+	return greeting
+}
+
+func main() {
+
+	var lang string
+
+	flag.StringVar(&lang, "lang", "en", "The required language, e.g. en, ur...")
+
+	flag.Parse()
+
+	greeting := greet(language(lang))
+
+	fmt.Println(greeting)
 }
